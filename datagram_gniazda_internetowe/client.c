c76c92a58c9a9238b382a57f21345351d4bc4e43
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2022-08-19 19:28:52 +0200

    eh

--- datagram_gniazda_internetowe/client.c
+++ datagram_gniazda_internetowe/client.c
@@ -1,40 +1,31 @@
-
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <sys/un.h>
 #include <netinet/ip.h>
-/*
-   man 7 ip - pokazana struktura sockaddr_in
-   man sendmmsg pokazany htonl(INADDR_LOOPBACK)
-   man sendto
-*/
-int main(int argc, char *argv[])
-{
+#include <string.h>
 
-   int    serverSocket;
-   struct sockaddr_in serverAddress;
+int main(int argc, char * argv[]){
+   
+   struct sockaddr_in server_addr;
 
-   serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-   if (serverSocket == -1)
-   {
-      perror("socket() failed");
+        int sfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sfd == -1){
+      perror("socket");
+      return -1;
+    }
+    memset(&server_addr, 0, sizeof(server_addr));
+
+   server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        server_addr.sin_port = htons(1234);
+   
+   if(sendto(sfd, argv[1], 50, 0, (struct sockaddr *) &server_addr,
+            sizeof(server_addr)) == -1){
+      perror("sendto");
       return -1;
    }
 
-   serverAddress.sin_family = AF_INET; 
-   serverAddress.sin_port = 8080;
-   serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-
-
-   if(sendto(serverSocket, argv[1], 100, 0, (struct sockaddr *)&serverAddress,sizeof(serverAddress)) == -1)
-   {
-      perror("send() failed");
-      return -1;
-   }
-
-   close(serverSocket);
    return 0;
 }
--- datagram_gniazda_internetowe/server.c
+++ datagram_gniazda_internetowe/server.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <sys/un.h>
-#include <netdb.h>
+#include <netinet/ip.h>
+#include <string.h>
 
-/*
-man recvmmsg - w przykladzie jest uzyta struktura sockaddr_in
-man recvfrom
-*/
+int sfd, cfd;
 
-#define BUFFER_LENGTH    250
+void signalHandler(int sig){
+   printf("\nserver stop\n");
+   close(sfd);
+   close(cfd);
+   exit(0);
+}
 
+int main(){
+   signal(SIGINT, signalHandler);
+   struct sockaddr_in server_addr, client_addr;
+        socklen_t client_addr_size = sizeof(client_addr);
 
-int main()
-{
-   int    serverSocket;
-   char   buffer[BUFFER_LENGTH]; //tutaj bedzie to co dostaniemy od klienta
-   struct sockaddr_in serverAddressStruct; //potrzebna struktura
+        sfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sfd == -1){
+      perror("socket");
+      return -1;
+    }
+    memset(&server_addr, 0, sizeof(client_addr));
 
+   server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        server_addr.sin_port = htons(1234);
 
-   serverSocket = socket(AF_INET, SOCK_DGRAM, 0); 
-   if (serverSocket == -1)
-   {
-      perror("socket() failed");
+   if(bind(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1){
+      perror("bind");
       return -1;
    }
-
-   serverAddressStruct.sin_family = AF_INET; //man 7 ip
-   serverAddressStruct.sin_port = 8080;
-   serverAddressStruct.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // man sendmmsg
-
-   if(bind(serverSocket, (struct sockaddr *)&serverAddressStruct, sizeof(serverAddressStruct)) == -1)
-   {
-      perror("bind() failed");
-      return -1;
-   }
-
+   char fromClient[50];
    while(1){
-      socklen_t len = sizeof serverAddressStruct;
-      if (recvfrom(serverSocket, buffer, sizeof(buffer), 0,(struct sockaddr*)&serverAddressStruct, &len) == -1)
-      {
-         perror("recv() failed");
-         break;
-      } 
-      printf("%s\n",buffer);
-   }
-   
+      if(recvfrom(sfd, fromClient, 50, 0, (struct sockaddr *) &client_addr,
+            &client_addr_size) == -1){
+         perror("recvfrom");
+         return -1;
+      }
 
-   close(serverSocket);
-   return 0;
+      printf("from client: %s\n");
+   }
+
+
+
 }
